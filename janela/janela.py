c9724b110213b319c8d1e8bdcb697fd73f1a0d25
Marcos Souza <marcossouza897@gmail.com>
2022-08-23 15:48:02 -0300

    adicionado Label

--- janela/janela.py
+++ janela/janela.py
@@ -6,7 +6,6 @@ class janela(QMainWindow):
     def __init__(self):
         super().__init__() 
 
-
         self.topo = 100
         self.esquerda = 100
         self.largura = 800
@@ -19,15 +18,21 @@ class janela(QMainWindow):
         botao1.setStyleSheet('QPushButton {background-color:#2268ff;font:bold;font-size:20px}')
         botao1.clicked.connect(self.botao1_click)
 
-        botao2 = QPushButton("Botao 1", self)
+        botao2 = QPushButton("Botao 2", self)
         botao2.move(350,200)
         botao2.resize(150,80)
         botao2.setStyleSheet('QPushButton {background-color:#2268ff;font:bold;font-size:20px}')
-        botao2.clicked.connect(self.botao1_click)
+        botao2.clicked.connect(self.botao2_click)
+
+        self.label_1 = QLabel(self)
+        self.label_1.setText('Aperte um botão !!')
+        self.label_1.move(50,50)
+        self.label_1.setStyleSheet('QLabel {font:bold;font-size:25px;color:"blue"}')
+        self.label_1.resize(600,80)
 
         self.CarregarJanela()
 
-    
+        
 
     def CarregarJanela(self):
         self.setGeometry(self.esquerda, self.topo, self.largura,self.altura)
@@ -35,4 +40,13 @@ class janela(QMainWindow):
         self.show()
 
     def botao1_click(self):
-        print('click, click e click')
+        self.label_1.setText("o botão 1 foi clicado")
+        self.label_1.setStyleSheet('QLabel {font:bold;font-size:25px;color:"yellow"}')
+    
+    def botao2_click(self):
+        self.label_1.setText("o botão 2 foi clicado")
+        self.label_1.setStyleSheet('QLabel {font:bold;font-size:25px;color:"red"}')   
+aplicacao = QApplication(sys.argv)
+
+j=janela()
+sys.exit(aplicacao.exec_())
