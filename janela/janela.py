e334784e392fdbae3e034dd3cbb6c62a4ea9e732
Marcos Souza <marcossouza897@gmail.com>
2022-08-23 14:46:57 -0300

    adicionado a função botão

--- janela/janela.py
+++ janela/janela.py
@@ -1,23 +1,38 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from tkinter import font
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QLabel
 
 class janela(QMainWindow):
     def __init__(self):
         super().__init__() 
+
 
         self.topo = 100
         self.esquerda = 100
         self.largura = 800
         self.altura = 600
         self.titulo = "primeira janela"
+
+        botao1 = QPushButton("Botao 1", self)
+        botao1.move(150,200)
+        botao1.resize(150,80)
+        botao1.setStyleSheet('QPushButton {background-color:#2268ff;font:bold;font-size:20px}')
+        botao1.clicked.connect(self.botao1_click)
+
+        botao2 = QPushButton("Botao 1", self)
+        botao2.move(350,200)
+        botao2.resize(150,80)
+        botao2.setStyleSheet('QPushButton {background-color:#2268ff;font:bold;font-size:20px}')
+        botao2.clicked.connect(self.botao1_click)
+
         self.CarregarJanela()
+
+    
 
     def CarregarJanela(self):
         self.setGeometry(self.esquerda, self.topo, self.largura,self.altura)
         self.setWindowTitle(self.titulo)
         self.show()
-    
-aplicacao = QApplication(sys.argv)
 
-j=janela()
-sys.exit(aplicacao.exec_())
+    def botao1_click(self):
+        print('click, click e click')
